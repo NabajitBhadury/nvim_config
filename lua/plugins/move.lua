91a5ae6845b55fa3b4fff35327c9a6ec594323b3
NabajitBhadury <nabajitbhadury@gmail.com>
2023-09-10 21:19:57 +0530

    changed ctrl to alt for moving lines

--- lua/plugins/move.lua
+++ lua/plugins/move.lua
@@ -1,11 +1,10 @@
 return {
     "fedepujol/move.nvim",
     config = function ()
-        vim.api.nvim_set_keymap('n', '<C-j>', ':MoveLine(1)<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', '<C-k>', ':MoveLine(-1)<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<A-j>', ':MoveLine(1)<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap('n', '<A-k>', ':MoveLine(-1)<CR>', { noremap = true, silent = true })
         vim.api.nvim_set_keymap('n', '<leader>wf', ':MoveWord(1)<CR>', { noremap = true, silent = true })
         vim.api.nvim_set_keymap('n', '<leader>wb', ':MoveWord(-1)<CR>', { noremap = true, silent = true })
     end
 }
-
 

b367d9fe214a3a34d71140e188261b973bb1a96e
NabajitBhadury <nabajitbhadury@gmail.com>
2024-02-26 21:36:15 +0530

    lualine updated

--- lua/plugins/flutter_tools.lua
+++ lua/plugins/flutter_tools.lua
@@ -1,30 +1,39 @@
 return {
-    'akinsho/flutter-tools.nvim',
-    lazy = true,
-    ft = { 'dart' },
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim',
-    },
-    config = function()
-        local m = require "configs.lsp.handler"
-        require('flutter-tools').setup {
-            lsp = {
-                on_attach = m.on_attach,
-                capabilities = m.capabilities,
-            },
-            decorations = {
-                statusline = {
-                    app_version = true,
-                    device = true,
-                },
-            },
-            widget_guides = {
-                enabled = true,
-            },
-        }
+  'akinsho/flutter-tools.nvim',
+  lazy = true,
+  ft = { 'dart' },
+  priority = 500,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'stevearc/dressing.nvim',
+  },
+  config = function()
+    local m = require "configs.lsp.handler"
+    require('flutter-tools').setup {
+      lsp = {
+        color = { -- show the derived colours for dart variables
+          enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+          background = true, -- highlight the background
+          background_color = { r = 19, g = 17, b = 24 }, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+          foreground = false, -- highlight the foreground
+          virtual_text = true, -- show the highlight using virtual text
+          virtual_text_str = "■", -- the virtual text character to highlight
+        },
+        on_attach = m.on_attach,
+        capabilities = m.capabilities,
+      },
+      decorations = {
+        statusline = {
+          app_version = true,
+          device = true,
+        },
+      },
+      widget_guides = {
+        enabled = true,
+      },
+    }
 
-        require("telescope").load_extension("flutter")
-        vim.keymap.set('n', "<leader>fc", ":Telescope flutter commands<CR>", { silent = true })
-    end,
+    require("telescope").load_extension("flutter")
+    vim.keymap.set('n', "<leader>fc", ":Telescope flutter commands<CR>", { silent = true })
+  end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,30 +1,8 @@
-return  {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	  "nvim-tree/nvim-web-devicons",
-	},
-	event = "VeryLazy",
-
-
-config = function()
-	require("lualine").setup({
-	  options = {
-		  theme = "auto",
-		  globalstatus = true,
-		  disabled_filetypes = { statusline = { "starter" } },
-	  },
-	  sections = {
-		  lualine_a = { "mode" },
-		  lualine_b = { "branch", "diff", "diagnostics" },
-		  lualine_c = {
-		  { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-		  { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-		  },
-		  lualine_x = { "searchcount", "encoding", "fileformat", "filetype" },
-		  lualine_y = { "progress" },
-		  lualine_z = { "location" },
-	  },
-	  extensions = { "lazy", "man", "nvim-dap-ui", "overseer", "quickfix", "toggleterm", "trouble" },
-	})
-end
+return {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require "configs.lualine"
+    end
 }
+
